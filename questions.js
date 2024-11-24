ba0453d3bde53b5f8536dadd9452d829e741d23a
William J Crowell <player6@gmail.com>
2024-11-23 19:56:55 -0700

    Create questions.js
    This file holds the questions data

---
+++ questions.js
@@ -0,0 +1,10 @@
+// questions.js
+const urls = {
+    easy: "questions/easy.json",
+    medium: "questions/medium.json",
+    hard: "questions/hard.json",
+    veryHard: "questions/very hard.json",
+    expert: "questions/expert.json",
+    satiricalEasy: "questions/satirical easy.json",
+    satiricalMedium: "questions/satirical medium.json"
+};
